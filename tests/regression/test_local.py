cf0b8c62314f81f43ebe72916cd5488de04c3f15
acesanderson <brian.briananderson@gmail.com>
2025-10-24 00:17:20 -0500

    updating regression tests, remotemodel implemented, progress works

--- tests/regression/test_local.py
+++ tests/regression/test_local.py
@@ -1,14 +1,15 @@
-from Chain.model.model_client import ModelClient
+from conduit.model.remote_model import RemoteModel
+from conduit.sync import Verbosity
+
+VERBOSITY = Verbosity.COMPLETE
 
 
 def test_local_sync():
-    from Chain.result.response import Response
+    from conduit.result.response import Response
 
-    model = ModelClient("llama3.1:latest")
-    response = model.query("name ten cetaceans", cache=False)
+    model = RemoteModel("llama3.1:latest")
+    response = model.query("name ten cetaceans", cache=False, verbose=VERBOSITY)
     content = str(response.content)
     assert isinstance(response, Response), f"Expected Response, got {type(response)}"
     assert len(content) > 0, "Response content is empty"
-
-
-print("hi bitch")
+    print(content)
