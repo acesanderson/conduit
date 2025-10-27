0f6d852d1c1149c854469e45db828969fca9c318
acesanderson <brian.briananderson@gmail.com>
2025-10-26 19:45:39 -0500

    moved out old old regression tests

--- tests/old/test_display.py
+++ tests/old/test_display.py
@@ -1,5 +1,7 @@
-from Chain import Model, Verbosity, ModelAsync, AsyncChain
-from Chain.tests.fixtures import sample_prompt_strings
+from conduit.sync import Model, Verbosity, ModelAsync, AsyncChain
+from conduit.batch import ModelAsync, AsyncChain
+from conduit.examples.fixtures import sample_prompt_strings
+
 
 def test_plain_sync_response():
     model = Model("gpt3")
@@ -7,21 +9,25 @@ def test_plain_sync_response():
         print(f"Testing Verbosity level: {level.name}")
         response = model.query("What is 2+2?", verbose=level)
 
+
 def test_plain_sync_error():
     model = Model("gpt3")
     for level in Verbosity:
         print(f"Testing Verbosity level: {level.name}")
         error = model.query("What is 2+2?", verbose=level, return_error=True)
 
+
 def test_plain_async_response():
     modelasync = ModelAsync("gpt3")
     chainasync = AsyncChain(model=modelasync)
     for level in Verbosity:
         print(f"Testing Verbosity level: {level.name}")
-        responses = chainasync.run(prompt_strings = sample_prompt_strings)
+        responses = chainasync.run(prompt_strings=sample_prompt_strings)
+
 
 def test_rich_sync_response():
     from rich.console import Console
+
     console = Console()
     Model._console = console
     # Tests
@@ -30,8 +36,10 @@ def test_rich_sync_response():
         print(f"Testing Verbosity level: {level.name}")
         response = model.query("What is 2+2?", verbose=level)
 
+
 def test_rich_sync_error():
     from rich.console import Console
+
     console = Console()
     Model._console = console
     model = Model("gpt3")
@@ -39,13 +47,14 @@ def test_rich_sync_error():
         print(f"Testing Verbosity level: {level.name}")
         error = model.query("What is 2+2?", verbose=level, return_error=True)
 
+
 def test_rich_async_response():
     from rich.console import Console
+
     console = Console()
     ModelAsync._console = console
     modelasync = ModelAsync("gpt3")
     chainasync = AsyncChain(model=modelasync)
     for level in Verbosity:
         print(f"Testing Verbosity level: {level.name}")
-        responses = chainasync.run(prompt_strings = sample_prompt_strings)
-
+        responses = chainasync.run(prompt_strings=sample_prompt_strings)
