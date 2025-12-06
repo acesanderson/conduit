55f1750f232f2529457ae95e5bc5ae9ef077e310
acesanderson <brian.briananderson@gmail.com>
2025-12-05 19:09:25 -0600

    caching decorator optimized + tested

---
+++ src/conduit/_dev/mocks.py
@@ -0,0 +1,24 @@
+from conduit.domain.result.response import Response
+from conduit.domain.message.message import AssistantMessage
+from conduit.domain.result.response_metadata import ResponseMetadata, StopReason
+from conduit.domain.request.generation_params import GenerationParams
+from conduit.domain.request.request import Request
+
+mock_params = GenerationParams(model="gpt3")
+mock_request = Request(
+    params=mock_params,
+    messages=[],
+)
+mock_response_metadata = ResponseMetadata(
+    model_slug="gpt3",
+    duration=1.23,
+    input_tokens=10,
+    output_tokens=20,
+    stop_reason=StopReason.STOP,
+)
+mock_assistant_message = AssistantMessage(content="Hello, world!")
+mock_response = Response(
+    request=mock_request,
+    message=mock_assistant_message,
+    metadata=mock_response_metadata,
+)
--- src/conduit/middleware/__init__.py
+++ src/conduit/middleware/__init__.py
@@ -1,9 +1,9 @@
-from conduit.middleware.caching import cache
+from conduit.middleware.caching import cache_sync
 from conduit.middleware.reporting import progress
 from conduit.middleware.telemetry import odometer
 
 __all__ = [
-    "cache",
+    "cache_sync",
     "odometer",
     "progress",
 ]
