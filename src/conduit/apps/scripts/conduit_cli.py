544cf3191787470a49d218f06c395ed056978716
acesanderson <brian.briananderson@gmail.com>
2025-12-21 19:40:55 -0600

    scaffolding of CLI app now works like a charm; need to update query function (WIP)

--- src/conduit/apps/scripts/conduit_cli.py
+++ src/conduit/apps/scripts/conduit_cli.py
@@ -2,8 +2,12 @@ from conduit.apps.cli.commands.base import BaseCommands
 from conduit.apps.cli.cli_class import ConduitCLI
 
 
-if __name__ == "__main__":
+def main():
     conduit_cli = ConduitCLI()
     commands = BaseCommands()
     conduit_cli.attach(commands)
     conduit_cli.run()
+
+
+if __name__ == "__main__":
+    main()
