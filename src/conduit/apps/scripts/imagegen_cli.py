9d1c28c7c7e58f395de439df2c6af7be6f2c434b
acesanderson <brian.briananderson@gmail.com>
2026-02-23 15:01:12 -0600

    many fixes to imagegen to get google imagegen models to work

--- src/conduit/apps/scripts/imagegen_cli.py
+++ src/conduit/apps/scripts/imagegen_cli.py
@@ -1,59 +1,20 @@
-import os
-from io import BytesIO
-import google.generativeai as genai
-from PIL import Image
+from __future__ import annotations
 import argparse
-from conduit.domain.message.imagemessage import ImageMessage
-import base64
+from conduit.core.model.model_sync import ModelSync
 
-
-def generate_image(prompt: str) -> ImageMessage:
-    genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-
-    # model = genai.GenerativeModel("gemini-2.5-flash-image")
-    # image = model.generate_content(prompt)
-    # save_image_from_response(image, "generated_image.png")
-    # text_response, image_response = image.parts
-    # text_response = text_response.text
-
-    model = genai.GenerativeModel("gemini-3-pro-image-preview")
-
-    image = model.generate_content(prompt)
-    save_image_from_response(image, "generated_image.png")
-
-    image_response = image.parts
-    text_response = ""
-
-    # convert image_response.inline_data.data from bytes to base64 string
-    base64_image_data = base64.b64encode(image_response.inline_data.data).decode(
-        "utf-8"
-    )
-    image_message = ImageMessage(
-        role="assistant",
-        text_content=text_response,
-        image_content=base64_image_data,
-    )
-    return image_message
-
-
-def save_image_from_response(response, filename):
-    for c in getattr(response, "candidates", []):
-        for p in getattr(c.content, "parts", []):
-            if getattr(p, "inline_data", None):
-                img = Image.open(BytesIO(p.inline_data.data))
-                img.save(filename)
-                return filename
-    raise RuntimeError("No image returned")
+# DEFAULT_MODEL = "imagegen"
+DEFAULT_MODEL = "gemini-2.5-flash-image"
 
 
 def main():
     parser = argparse.ArgumentParser(description="Generate an image from a prompt.")
-    parser.add_argument(
-        "prompt", type=str, help="The prompt to generate the image from."
-    )
+    parser.add_argument("prompt", type=str)
+    parser.add_argument("--model", "-m", default=DEFAULT_MODEL)
     args = parser.parse_args()
-    image_msg = generate_image(args.prompt)
-    image_msg.display()
+
+    model = ModelSync(model=args.model)
+    response = model.image.generate(prompt_str=args.prompt)
+    response.display()
 
 
 if __name__ == "__main__":
